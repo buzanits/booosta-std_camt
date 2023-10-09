9901bb84e261ad829d23d1c812955df8977000e6
Peter Buzanits <p.buzanits@epb.at>
2023-10-09 19:28:57 +0200

    added getTransaction()

--- src/Std_camt.php
+++ src/Std_camt.php
@@ -14,6 +14,7 @@ class Std_camt extends \booosta\base\Module
   use moduletrait_std_camt;
 
   protected $messageFormat = 'default';
+  protected $errors = [];
 
 
   public function __construct(protected $xmlfile = null) {}
@@ -39,5 +40,73 @@ class Std_camt extends \booosta\base\Module
 
     return $entries;
   }
+
+  public function getTransactions($xmlfile = null)
+  {
+    $result = [];
+    $entries = $this->parseFile($xmlfile);
+
+    foreach($entries as $day):
+      foreach($day as $transaction):
+        $data = [];
+
+        $data['record_id'] = $transaction?->getRecord()?->getId();
+        $data['date'] = $transaction?->getBookingDate()?->format('Y-m-d');
+        $data['datetime'] = $transaction?->getBookingDate()?->format('Y-m-d H:i:s');
+        $data['timezone'] = $transaction?->getBookingDate()?->getTimezone()?->getName();
+        $data['amount_cent'] = $transaction?->getAmount()?->getAmount();
+        $data['amount'] = intval($transaction?->getAmount()?->getAmount()) / 100;
+        $data['currency'] = $transaction?->getAmount()?->getCurrency()?->getCode();
+
+        $details = $transaction?->getTransactionDetails();
+        if(!is_array($details)):
+          $this->add_error("No details found in transaction with record id $record_id");
+          continue;
+        endif;
+
+        if(sizeof($details) != 1):
+          $this->add_error("More than one details found in transaction with record id $record_id");
+          continue;
+        endif;
+
+        $detail = $details[0];
+        $data['transaction_id'] = $detail?->getReference()?->getTransactionId();
+
+        $party0 = $detail?->getRelatedParties()[0];
+        $party1 = $detail?->getRelatedParties()[1];
+
+        if(is_a($party0?->getRelatedPartyType(), "Genkgo\\Camt\\DTO\\Creditor")):
+          $creditor = $party0;
+          $debtor = $party1;
+        else:
+          $creditor = $party1;
+          $debtor = $party0;
+        endif;
+
+        $data['creditor_name'] = $creditor?->getRelatedPartyType()?->getName();
+        $address = $creditor?->getRelatedPartyType()?->getAddress()?->getAddressLines();
+        $data['creditor_address'] = is_array($address) ? implode(', ', $address) : $address;
+        $data['creditor_country'] = $creditor?->getRelatedPartyType()?->getAddress()?->getCountry();
+        $data['creditor_iban'] = $creditor?->getAccount()?->getIban()?->getIban();
+
+        $data['debtor_name'] = $debtor?->getRelatedPartyType()?->getName();
+        $address = $debtor?->getRelatedPartyType()?->getAddress()?->getAddressLines();
+        $data['debtor_address'] = is_array($address) ? implode(', ', $address) : $address;
+        $data['debtor_country'] = $debtor?->getRelatedPartyType()?->getAddress()?->getCountry();
+        $data['debtor_iban'] = $debtor?->getAccount()?->getIban()?->getIban();
+
+        $data['transaction_id'] = $detail?->getReference()?->getTransactionId();
+        $data['message'] = $detail?->getRemittanceInformation()?->getMessage();
+        $data['is_credit'] = $detail?->getCreditDebitIndicator() == 'CRDT'; 
+
+        $result[] = $data;
+      endforeach;
+    endforeach;
+
+    return $result;
+  }
+
+  protected function add_error($msg) { $this->errors[] = $msg; }
+  public function get_errors() { return $this->errors; }
 }
 
