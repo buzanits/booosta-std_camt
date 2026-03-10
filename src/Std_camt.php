607fa4f5c3ef333d1378fb792f77adeeee57786a
Peter Buzanits <p.buzanits@epb.at>
2026-03-10 16:24:10 +0100

    Fixed several issues with current CAMT standard

--- src/Std_camt.php
+++ src/Std_camt.php
@@ -55,32 +55,46 @@ class Std_camt extends \booosta\base\Module
   {
     $result = [];
     $entries = $this->parseFile($xmlfile);
+    #b::debug($this->error());
 
     foreach($entries as $day):
       foreach($day as $transaction):
         $data = [];
 
-        $data['record_id'] = $transaction?->getRecord()?->getId();
+        $data['record_id'] = $record_id = $transaction?->getRecord()?->getId();
         $data['date'] = $transaction?->getBookingDate()?->format('Y-m-d');
         $data['datetime'] = $transaction?->getBookingDate()?->format('Y-m-d H:i:s');
         $data['timezone'] = $transaction?->getBookingDate()?->getTimezone()?->getName();
         $data['amount_cent'] = $transaction?->getAmount()?->getAmount();
         $data['amount'] = intval($transaction?->getAmount()?->getAmount()) / 100;
         $data['currency'] = $transaction?->getAmount()?->getCurrency()?->getCode();
+        #b::debug($data);
 
         $details = $transaction?->getTransactionDetails();
         if(!is_array($details)):
           $this->add_error("No details found in transaction with record id $record_id");
+          b::debug("No details ({$data['amount']})");
           continue;
         endif;
 
-        if(sizeof($details) != 1):
+        if(sizeof($details) > 1):
           $this->add_error("More than one details found in transaction with record id $record_id");
+          b::debug("Multiple details ({$data['amount']})"); b::debug($details);
+          continue;
+        endif;
+
+        if(sizeof($details) == 0):
+          #b::debug($transaction);
+          $data['transaction_id'] = $this->getUniqueID($transaction, null);
+          $data['message'] = $transaction?->getAdditionalInfo() ?: 'No details provided';
+          $data['is_credit'] = floatval($data['amount']) > 0;
+
+          $result[] = $data;
           continue;
         endif;
 
         $detail = $details[0];
-        $data['transaction_id'] = $detail?->getReference()?->getTransactionId();
+        #$data['transaction_id'] = $detail?->getReference()?->getTransactionId();
 
         $party0 = $detail?->getRelatedParties()[0];
         $party1 = $detail?->getRelatedParties()[1];
@@ -109,7 +123,7 @@ class Std_camt extends \booosta\base\Module
         $account = $debtor?->getAccount();
         if(is_callable([$account, 'getIban'])) $data['debtor_iban'] = $account->getIban()?->getIban();
 
-        $data['transaction_id'] = $detail?->getReference()?->getTransactionId();
+        $data['transaction_id'] = $this->getUniqueID($transaction, $detail);
         $data['message'] = $detail?->getRemittanceInformation()?->getMessage();
         $data['is_credit'] = $detail?->getCreditDebitIndicator() == 'CRDT'; 
 
@@ -118,6 +132,15 @@ class Std_camt extends \booosta\base\Module
     endforeach;
 
     return $result;
+  }
+
+  protected function getUniqueID($transaction, $detail)
+  {
+    #b::debug("txid: " . $detail?->getReference()?->getTransactionId());
+    #b::debug("srvid: " . $detail?->getReference()?->getAccountServicerTransactionId());
+    if($id = $detail?->getReference()?->getTransactionId()) return $id;
+    if($id = $transaction?->getAccountServicerReference()) return $id;
+    return null;
   }
 
   protected function add_error($msg) { $this->errors[] = $msg; }
